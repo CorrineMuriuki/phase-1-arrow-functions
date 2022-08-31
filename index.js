e482e3bfca0c55fb1232e0d0d8d3a60319ceadf1
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-08-31 17:16:48 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (x,y) => 2000/100;
+
+//arrow function called square, return keyword is optional as it makes a return anyway
+const square = a => a * a;
+
+const add = (a,b) => 3 + 4;
